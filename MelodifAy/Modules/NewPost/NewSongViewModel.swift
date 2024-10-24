ff665cc9c93241ac31b6c82c35b119afbe6d81f0
İbrahim Ay <ibrahimay@Ibrahim-MacBook-Air.local>
2024-10-24 18:36:00 +0300

    Updates to audio recording and video screens

--- MelodifAy/Modules/NewPost/NewSongViewModel.swift
+++ MelodifAy/Modules/NewPost/NewSongViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import Firebase
+
+class NewSongViewModel {
+    let firestore = Firestore.firestore()
+    
+    func saveAudioSketch(audioUrl: String, completion: @escaping (Bool) -> Void) {
+        guard let user = Auth.auth().currentUser else {
+            completion(false)
+            return
+        }
+        
+        let currentUserID = user.uid
+        
+        let audioSketchRef = firestore.collection("AudioSketch").document()
+        let firestoreSketch = ["audioUrl": audioUrl, "userID": currentUserID, "audioID": audioSketchRef.documentID] as [String: Any]
+        
+        audioSketchRef.setData(firestoreSketch) { error in
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+                completion(false)
+            } else {
+                print("Ses taslağı başarılı bir şekilde kaydedildi.")
+                completion(true)
+            }
+        }
+    }
+}
